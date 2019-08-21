409b066b83f7745780b74d8eebee2fef5e156b1b
yangshu <yang_shu@muorngtech.com>
2019-08-21 16:58:52 +0800

    tina 小混混,快学习算法

--- tina_src/sort/tina.java
+++ tina_src/sort/tina.java
@@ -4,7 +4,6 @@ package sort;
  * @author yangshu
  * @version 5.0.0
  * @created at 2019/8/21-4:44 PM
- * copyright @2019 Beijing Morong Information Techology CO.,Ltd.
  */
 public class tina {
     /**
