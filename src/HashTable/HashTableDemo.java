1cea0c0ccd7f176c5d00bb03517e4ae874d52d76
yangshu <yang_shu@muorngtech.com>
2019-08-25 00:21:39 +0800

    hashtable improve

--- src/HashTable/HashTableDemo.java
+++ src/HashTable/HashTableDemo.java
@@ -1,5 +1,7 @@
 package HashTable;
 
+import java.util.Scanner;
+
 /**
  * @author yangshu
  * @version 5.0.0
@@ -7,121 +9,221 @@ package HashTable;
  * copyright @2019 Beijing Morong Information Techology CO.,Ltd.
  */
 public class HashTableDemo {
-    /**
-     * hashtable is a key&value datastructure and comprise array and list.
-     * define list [] array to store lists, and list[index] = head of list and has a filed to point list
-     * list can be curd and curd has to locate the index of arr  before check in list.
-     * index = hashcode(key) % size. then node will be added in the following of list.
-     */
-    class HashTable{
-        private int size;
-        private ElementList [] list;
-        HashTable(int size){
-            this.size = size;
-            for(int i =0 ;i < list.length; i++){
-                list[i] = new ElementList(new Node());
+    public static void main(String[] args) {
+
+        //创建哈希表
+        HashTable hashTab = new HashTable(7);
+
+        //写一个简单的菜单
+        String key = " ";
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("add:  添加雇员");
+            System.out.println("list: 显示雇员");
+            System.out.println("find: 查找雇员");
+            System.out.println("exit: 退出系统");
+
+            key = scanner.next();
+            switch (key) {
+                case "add":
+                    System.out.println("输入id");
+                    int id = scanner.nextInt();
+                    //创建 雇员
+                    Node emp = new Node(id);
+                    hashTab.add(emp);
+                    break;
+                case "list":
+                    hashTab.list();
+                    break;
+                case "find":
+                    System.out.println("请输入要查找的id");
+                    id = scanner.nextInt();
+                    hashTab.select(id);
+                    break;
+                case "exit":
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    break;
             }
-        }
-        void add(Node newNode){
-            int index = locate(newNode.key);
-
-            list[index].add(newNode);
-
-        }
-        void delete(Integer key){
-
-        }
-        Node select(Integer key){
-            Node element = null;
-            return element;
-        }
-        boolean update(Integer key){
-            return true;
-        }
-        int locate(Integer key){
-            return (int)key % this.size;
         }
 
     }
-    class ElementList{
-        private Node head;
-        ElementList(Node head){
-            this.head = head;
-        }
-        void add(Node newNode){
-           if(isEmpty(head)){
-                head.next = newNode;
-                return;
-           }
-           Node point = head.next;
-           while(point.next != null){
-               point = point.next;
-           }
-           point.next = newNode;
 
-        }
-        void remove(Integer key){
-            if(head.next.next == null ){
-                if(head.next.key == key){
-                    head.next = null;
-                }
-            }
-            if(isEmpty(head)){
-                return;
-            }
-            Node point = head.next;
-            Node pre = head;
-            while(point.next!= null){
-                if(point.key == key){
-                    pre.next = point.next;
-                }
-                point = point.next;
-            }
+}
 
+/**
+ * hashtable is a key&value datastructure and comprise array and list.
+ * define list [] array to store lists, and list[index] = head of list and has a filed to point list
+ * list can be curd and curd has to locate the index of arr  before check in list.
+ * index = hashcode(key) % size. then keyde will be added in the following of list.
+ */
+class HashTable{
+    /***
+     * define a hashtable
+     * filed: size, Array[] store all Node head, and head has a next filed to point at the list
+     *
+     */
+    private int size;
+    private ElementList [] list;
+
+    /***
+     * get size and define a set of Node head, and set in Array[index]
+     * @param size
+     */
+    HashTable(int size){
+        this.size = size;
+        list = new ElementList[this.size];
+        for(int i =0 ;i < size; i++){
+            list[i] = new ElementList(new Node(111));
         }
-        Node select(int key, Node head){
-            if(isEmpty(head)){
-                return null;
+    }
+
+    /***
+     * add new keyde to hashtable, and will call the add method from array class.
+     * @param newNode
+     */
+    void add(Node newNode){
+        int index = locate(newNode.key);
+
+        list[index].add(newNode);
+
+    }
+    void delete(Integer key){
+
+    }
+    Node select(Integer key){
+        Node result = list[locate(key)].select(key);
+        if(result != null) {//找到
+            System.out.printf("在第%d条链表中找到 雇员 id = %d\n", ( locate(key)+ 1), key);
+        }else{
+            System.out.println("在哈希表中，没有找到该雇员~");
+        }
+        return result;
+    }
+
+    public void list() {
+        for(int i = 0; i < size; i++) {
+            list[i].printList(i);
+        }
+    }
+
+    boolean update(Integer key){
+        return true;
+    }
+
+    int locate(Integer key){
+        return (int)key % this.size;
+    }
+
+}
+
+class Node{
+    public Integer key;
+    public int value;
+    public Node next;
+    public Node(){
+
+    }
+    public Node(int key){this.key = key;}
+    public Node(Integer key, int value, Node next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class ElementList{
+    private Node head;
+    ElementList(Node head){
+        this.head = head;
+    }
+    void add(Node newNode){
+        if(isEmpty()){
+            head.next = newNode;
+            return;
+        }
+        Node point = head.next;
+        while(point.next != null){
+            point = point.next;
+        }
+        point.next = newNode;
+
+    }
+    void remove(Integer key){
+        if(head.next.next == null ){
+            if(head.next.key == key){
+                head.next = null;
             }
-            Node point = head.next;
-            while(point.next != null){
-                if(key == point.key){
-                    return point;
-                }
+        }
+        if(isEmpty()){
+            return;
+        }
+        Node point = head.next;
+        Node pre = head;
+        while(point.next!= null){
+            if(point.key == key){
+                pre.next = point.next;
             }
+            point = point.next;
+        }
+
+    }
+    Node select(int key){
+        if(isEmpty()){
+            System.out.println("链表为空");
+
             return null;
         }
-        boolean update(Integer key, Integer value, Node head){
-
-            Node point = head;
-            while(point.next != null){
-                if(point == head){
-                    continue;
-                }
-                if(point.key == key){
-                    point.value = value;
-                    return true;
-                }
-                point = point.next;
+        Node point = head.next;
+        while(point != null){
+            if(key == point.key){
+                return point;
             }
-            return false;
-
+            point = point.next;
         }
+        return null;
+    }
+    boolean update(Integer key, Integer value, Node head){
 
-        boolean isEmpty(Node head){
-            if(head.next == null){
+        Node point = head;
+        while(point.next != null){
+            if(point == head){
+                continue;
+            }
+            if(point.key == key){
+                point.value = value;
                 return true;
             }
-            return false;
+            point = point.next;
         }
-
-
-
-    }
-    class Node{
-        private Integer key;
-        private int value;
-        private Node next;
+        return false;
 
     }
+
+    boolean isEmpty(){
+        if(head.next == null){
+            return true;
+        }
+        return false;
+    }
+    public void printList(int key) {
+        if(this.head.next == null) { //说明链表为空
+            System.out.println("第 "+(key+1)+" 链表为空");
+            return;
+        }
+        System.out.print("第 "+(key+1)+" 链表的信息为");
+        Node curEmp = head; //辅助指针
+        while(true) {
+            System.out.printf(" => id=%d name=%s\t", curEmp.key,curEmp.value);
+            if(curEmp.next == null) {//说明curEmp已经是最后结点
+                break;
+            }
+            curEmp = curEmp.next; //后移，遍历
+        }
+        System.out.println();
+    }
+
+
+
 }
