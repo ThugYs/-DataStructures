b6adec55de83ef90b27b75849390d71183e5ce7b
yangshu <yang_shu@muorngtech.com>
2019-08-27 11:26:36 +0800

    modify

---
+++ src/binaryTree/AVLTree.java
@@ -0,0 +1,10 @@
+package binaryTree;
+
+/**
+ * @author yangshu
+ * @version 5.0.0
+ * @created at 2019/8/26-6:52 PM
+ * copyright @2019 Beijing Morong Information Techology CO.,Ltd.
+ */
+public class AVLTree {
+}
