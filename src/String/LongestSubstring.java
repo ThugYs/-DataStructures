f0952c96e7192b8c7cb5ddea3d98c7470db3e96b
Shu Yang <ys1127vip@163.com>
2019-09-05 01:14:37 +0800

    Longest SubString without Repeated Character Final version

--- src/String/LongestSubstring.java
+++ src/String/LongestSubstring.java
@@ -114,4 +114,23 @@ public class LongestSubstring {
         }
         return maxLength;
     }
+
+    /***
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstringFinal(String s) {
+        int n = s.length(), ans = 0;
+        // 桶的思想，有的话就是索引 ，没有就是
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+
 }
