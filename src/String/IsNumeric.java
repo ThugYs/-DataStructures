d7e5d3c57f69ca18be16dbc2be6635714ba6b52b
Shu Yang <ys1127vip@163.com>
2019-09-07 18:48:36 +0800

    isNumeric

--- src/String/IsNumeric.java
+++ src/String/IsNumeric.java
@@ -65,4 +65,58 @@ public class IsNumeric {
         }
         return false;
     }
+
+
+    public boolean isNumeric_3(char[] str) {
+        // 标记符号、小数点、e是否出现过
+        boolean sign = false, decimal = false, hasE = false;
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] == 'e' || str[i] == 'E') {
+                if (i == str.length - 1){
+                    return false; // e后面一定要接数字
+                }
+                if (hasE) {
+                    return false; // 不能同时存在两个e
+                }
+                hasE = true;
+            } else if (str[i] == '+' || str[i] == '-') {
+                // 第二次出现+-符号，则必须紧接在e之后
+                if (sign && str[i - 1] != 'e' && str[i - 1] != 'E'){
+                    return false;
+                }
+                // 第一次出现+-符号，且不是在字符串开头，则也必须紧接在e之后
+                if (!sign && i > 0 && str[i - 1] != 'e' && str[i - 1] != 'E'){
+                    return false;
+                }
+                sign = true;
+            } else if (str[i] == '.') {
+                // e后面不能接小数点，小数点不能出现两次
+                if (hasE || decimal){
+                    return false;
+                }
+                decimal = true;
+            } else if (str[i] < '0' || str[i] > '9') { // 不合法字符
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * 系统自身的校验机制
+     * @param str
+     * @return
+     */
+    public boolean isNumeric_2 (char[] string) {
+        try {
+            double number = Double.parseDouble(new String(str));
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+
+
 }
